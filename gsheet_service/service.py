d8c7ee429a43836485867496fc15a037136b5226
Beesama <gbozee@gmail.com>
2020-10-08 17:19:32 +0100

    save changes

--- gsheet_service/service.py
+++ gsheet_service/service.py
@@ -29,8 +29,22 @@ if not settings.DEBUG:
 else:
     config.update(private_key=json.loads(f'"{config["private_key"]}"'))
 
+async def read_row(link, sheet, key, value) -> Result:
+    if not link or not sheet:
+        return Result(error="Missing `link` or `sheet` value")
+    instance = models.GoogleSheetInterface(**config)
+    instance.load_file(link, sheet)
+    result = instance.get_all_records()
+    if value:
+        if not key:
+            return Result(error="Missing `key` field to read a single record")
+        found = [x for x in result if x[key] == value]
+        if found:
+            return Result(data=found[0])
+    return Result(data=result)
 
-async def read_row(link, sheet, page, page_size, key, value) -> Result:
+
+async def read_row_1(link, sheet, page, page_size, key, value) -> Result:
     if not link or not sheet:
         return Result(error="Missing `link` or `sheet` value")
     instance = models.GoogleSheetInterface(**config)
