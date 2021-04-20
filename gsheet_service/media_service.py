ca45fd259fbbb70ce2f4124ae071972f43eac977
Chidi Enwereji <chidibede@gmail.com>
2021-04-20 16:34:08 +0100

    Ability to add server confi (#9)
    * fix: added new endpoints leaving all the originals untouched
    
    * Create python-app-restore.yml
    
    * fix: Fixed the error with returning the range of rows for each page and optimized the speed by removing the get row count function from service.py
    
    * fix: created a get row range
    
    * fix: fixed the row index
    
    * fix: renamed the sheet_names to questions
    
    * fix: changed the default page size
    
    * fix: abstracted the get page into a function
    
    * Create python-app.yml
    
    * feat: added a new endpoint to account for referencing of cells in google sheet
    
    * fix: merge conflict
    
    * extended the upload image view to account for multipart/formdata
    
    * fixed undefined settings in media_utils.py
    
    * fix: test_media_views error
    
    * edited now.json
    
    * added video upload and audio services
    
    * reverted delete resource
    
    * fixed delete resource
    
    * removed print fxn

--- gsheet_service/media_service.py
+++ gsheet_service/media_service.py
@@ -15,11 +15,15 @@ async def get_credentials(_id, link=None, sheet=None, credentials=None, **kwargs
 
 
 async def create_cloudinary_image(identifier, **data) -> Result:
+    server_config = data.pop("server_config", {})
     link = data.pop("link", None) or settings.MEDIA_SPREADSHEET
     sheet = data.pop("sheet", None) or settings.MEDIA_SHEET_NAME
     image = data.pop("image", None)
     resource_type = data.pop("resource_type", None) or "image"
-    config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
+    if server_config:
+        config = server_config
+    else:
+        config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
     if image and config:
         result = media_utils.MediaServiceAPI.create_resource(
             image, config=config, resource_type=resource_type, **data
@@ -29,13 +33,35 @@ async def create_cloudinary_image(identifier, **data) -> Result:
         error="Error creating the image in cloudinary, missing image or invalid config"
     )
 
-
 async def create_cloudinary_video(identifier, **data) -> Result:
+    server_config = data.pop("server_config", {})
     link = data.pop("link", None) or settings.MEDIA_SPREADSHEET
     sheet = data.pop("sheet", None) or settings.MEDIA_SHEET_NAME
     video = data.pop("video", None)
     resource_type = data.pop("resource_type", None) or "video"
-    config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
+    if server_config:
+        config = server_config
+    else:
+        config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
+    if video and config:
+        result = media_utils.MediaServiceAPI.create_resource(
+            video, config=config, resource_type=resource_type, **data
+        )
+        return Result(data=result)
+    return Result(
+        error="Error creating the video in cloudinary, missing video or invalid config"
+    )
+
+async def create_cloudinary_video(identifier, **data) -> Result:
+    server_config = data.pop("server_config", {})
+    link = data.pop("link", None) or settings.MEDIA_SPREADSHEET
+    sheet = data.pop("sheet", None) or settings.MEDIA_SHEET_NAME
+    video = data.pop("video", None)
+    resource_type = data.pop("resource_type", None) or "video"
+    if server_config:
+        config = server_config
+    else:
+        config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
     if video and config:
         result = media_utils.MediaServiceAPI.create_resource(
             video, config=config, resource_type=resource_type, **data
@@ -47,11 +73,15 @@ async def create_cloudinary_video(identifier, **data) -> Result:
 
 
 async def create_cloudinary_audio(identifier, **data) -> Result:
+    server_config = data.pop("server_config", {})
     link = data.pop("link", None) or settings.MEDIA_SPREADSHEET
     sheet = data.pop("sheet", None) or settings.MEDIA_SHEET_NAME
     audio = data.pop("audio", None)
     resource_type = data.pop("resource_type", None) or "raw"
-    config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
+    if server_config:
+        config = server_config
+    else:
+        config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
     if audio and config:
         result = media_utils.MediaServiceAPI.create_resource(
             audio, config=config, resource_type=resource_type, **data
@@ -63,9 +93,13 @@ async def create_cloudinary_audio(identifier, **data) -> Result:
 
 
 async def get_cloudinary_url(identifier, kind, public_id, **data) -> Result:
+    server_config = data.pop("server_config", {})
     link = data.pop("link", None) or settings.MEDIA_SPREADSHEET
     sheet = data.pop("sheet", None) or settings.MEDIA_SHEET_NAME
-    config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
+    if server_config:
+        config = server_config
+    else:
+        config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
     if public_id and config:
         instance = media_utils.MediaServiceAPI.get_instance(
             resource_id=public_id, kind=kind, config=config
@@ -75,12 +109,15 @@ async def get_cloudinary_url(identifier, kind, public_id, **data) -> Result:
         error="Error getting the image in cloudinary, missing public_id or invalid config"
     )
 
-
 async def delete_cloudinary_resource(identifier, public_id, **data) -> Result:
+    server_config = data.pop("server_config", {})
     link = data.pop("link", None) or settings.MEDIA_SPREADSHEET
     sheet = data.pop("sheet", None) or settings.MEDIA_SHEET_NAME
     kind = data.pop("kind", None) or "image"
-    config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
+    if server_config:
+        config = server_config
+    else:
+        config = await get_provider_sheet(link=link, sheet=sheet, provider=identifier)
     if public_id and config:
         instance = media_utils.MediaServiceAPI.get_instance(
             resource_id=public_id, kind=kind, config=config
